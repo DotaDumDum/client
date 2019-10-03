05c218de96272169b6694bfce4c5dc728d51aa24
Ricky <audrickong.ao@gmail.com>
2019-10-03 19:24:05 +0700

    Layout Basic

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/heroes'
+    })
+    .done(heroes => {
+        heroes.forEach((hero,index) => {
+            $('#heroes').append(`
+            <div class="card" style="width: 18rem;">
+                <div class="card-body" id="hero${index}"">
+                    <a class="h2" href="#">${hero.localized_name}</a>
+                    <br>
+                    <a class="h5">${hero.attack_type}</a>
+                    <br>
+                </div>
+            </div>`)
+            hero.roles.forEach(role => {
+                $(`#hero${index}`).append(`<li>${role}</li>`)
+            })
+        })
+    })
+    .fail(console.log)
+})
